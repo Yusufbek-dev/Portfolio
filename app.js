7e12e84cce2b0691016897ff91293c0b5ac1f4f0
Yusufbek01 <Yusufbek1771@gmail.com>
2021-06-26 23:48:25 +0500

    bugs fixed

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const navSlide = () => {
        }else {
          link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
        }
-
     });
     // burger Animation
     burger.classList.toggle("toggle");
@@ -24,3 +23,42 @@ const navSlide = () => {
 }
 
 navSlide()
+// set  date 
+
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear()
+
+
+
+// smooth scroll
+
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link) {
+   link.addEventListener("click", function(e) {
+     // prevent default 
+     e.preventDefault();
+     // navigation to specific spot
+     const id = e.currentTarget.getAttribute("href").slice(1);
+     const element = document.getElementById(id);
+     let position = element.offsetTop;
+     window.scrollTo({
+       left:0,
+       top:position,
+     });
+    //  linksContainer.stlye.height =0;
+   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
